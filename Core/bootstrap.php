da029495b774093dae735b012be21e8a8d5222b9
Marcia Silva <contato.pix@gmail.com>
2019-05-07 16:46:55 -0300

    Inclusão do arquivo de rotas ao chamar a classe Router

--- Core/bootstrap.php
+++ Core/bootstrap.php
@@ -1,4 +1,8 @@
 <?php
+//Arquivo de rotas da aplicacao
+$routes = require_once __DIR__ . '/../App/routes.php';
+//Capturando a URL do browser
 $url = filter_input(INPUT_GET, 'url', FILTER_SANITIZE_SPECIAL_CHARS);
-$router = new \Core\Router($url);
+//Router(URLBrowser, Rotas);
+$router = new \Core\Router($url, $routes);
 $router->run();
