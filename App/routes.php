4c809e414ecd55d9b22d194c73c36347ff13341f
Marcia Silva <contato.pix@gmail.com>
2019-05-07 17:14:06 -0300

    Adicionadas as rotas para exibição de categoria de notícias e noticia single

--- App/routes.php
+++ App/routes.php
@@ -4,4 +4,6 @@
 return [
     //['rota', 'controller@action'],
     ['/', 'NoticiasController@index'],
+    ['/{cidade}', 'NoticiasController@category'],
+    ['/{cidade}/{slug}', 'NoticiasController@singlePost'],
 ];
