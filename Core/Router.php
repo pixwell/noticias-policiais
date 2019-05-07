88921e9b862d00b1fa8e7f076048a65108987c11
Marcia Silva <contato.pix@gmail.com>
2019-05-07 16:13:09 -0300

    Captura da URL do Browser e criação do método para processamento da entrada das rotas

--- Core/Router.php
+++ Core/Router.php
@@ -4,5 +4,15 @@ namespace Core;
 
 class Router
 {
+    private $url;
 
+    public function __construct($urlBrowser)
+    {
+        $this->url = $urlBrowser;
+    }
+
+    public function run()
+    {
+        echo 'URL: ' . $this->url;
+    }
 }
