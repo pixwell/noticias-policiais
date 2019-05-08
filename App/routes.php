1e03510efa4102dccd3664813923997e99287101
Marcia Silva <contato.pix@gmail.com>
2019-05-08 10:55:24 -0300

    Rotas para teste

--- App/routes.php
+++ App/routes.php
@@ -6,6 +6,6 @@ return [
     ['/', 'NoticiasController@index'],
     ['/{cidade}', 'NoticiasController@category'],
     ['{cidade}/{slug}/', 'NoticiasController@singlePost'],
-    ['/noticia/{slug}/', 'NoticiasController@singlePost'],
+    ['/noticia/{id}/show', 'NoticiasController@singlePost'],
     ['/cidade/{id}/', 'NoticiasController@singlePost'],
 ];
