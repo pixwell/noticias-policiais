94cd03320f748e5c8e093b6545931726c4582deb
Marcia Silva <contato.pix@gmail.com>
2019-05-08 14:39:52 -0300

    Alteração na estrutura da definição das rotas

--- App/routes.php
+++ App/routes.php
@@ -3,9 +3,9 @@
 //Rotas da aplicacao
 return [
     //['rota', 'controller@action'],
-    ['/', 'NoticiasController@index'],
-    ['/{cidade}', 'NoticiasController@category'],
-    ['{cidade}/{slug}/', 'NoticiasController@singlePost'],
-    ['/noticia/{id}/show', 'NoticiasController@singlePost'],
-    ['/cidade/{id}/', 'NoticiasController@singlePost'],
+    ['/', 'NoticiasController', 'index'],
+    ['/{cidade}', 'NoticiasController', 'category'],
+    ['{cidade}/{slug}/', 'NoticiasController', 'singlePost'],
+    ['/noticia/{id}/show', 'NoticiasController', 'singlePost'],
+    ['/cidade/{id}/', 'NoticiasController', 'singlePost'],
 ];
