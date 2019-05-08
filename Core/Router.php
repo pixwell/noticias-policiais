e408cd2ba2b7ad5f5ab028135bd9d45882aa38f5
Marcia Silva <contato.pix@gmail.com>
2019-05-08 15:05:13 -0300

    Inserido break na verificação IF para sair do laço ao achar a rota

--- Core/Router.php
+++ Core/Router.php
@@ -28,6 +28,8 @@ class Router
                 if( (count($routeArray) == count($urlArray)) &&  (strpos($routeArray[$i], '{') !== false)){
                     //Substituindo o item {}
                     $routeArray[$i] = $urlArray[$i];
+                    //Capturando os parametros
+                    $param[] = $urlArray[$i];
                 }
                 //Montar a URL novamente, agora com o parametro
                 $routeModified = implode('/', $routeArray);
@@ -35,7 +37,11 @@ class Router
             //Se a URL for igual à rota
             if( $this->url == $routeModified ){
                 echo '<b>Rota Modificada: </b>' . $routeModified . '<br>';
-                echo '<b>Açoes: </b>' . $route[1] . '<hr>';
+                echo '<b>Controller: </b>' . $route[1] . '<br>';
+                echo '<b>Action: </b>' . $route[2] . '<hr>';
+                echo '<b>Parametros: </b><br>';
+                echo '<pre>' . var_dump($param) . '</pre><hr>';
+                break; //Sai do laco ao achar a rota
             }
         }
     }
