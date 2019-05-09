eb9073a42a88def63c66ef1c799057b2205fd67e
Marcia Silva <contato.pix@gmail.com>
2019-05-09 15:21:47 -0300

    Método para inclusão do Code footer

--- Core/BaseController.php
+++ Core/BaseController.php
@@ -20,13 +20,14 @@ abstract class BaseController
         if( file_exists($filePath) ){
             $variables ? extract($variables) : null;
             $code_head = $this->get_header();
+            $code_footer = $this->get_footer();
             include_once $filePath;
         } else {
             http_response_code(404);
             throw new \Exception("View não encontrada.");
         }
     }
-
+    
     private function get_header()
     {
         $headCode = __DIR__ . '/../App/View/code_head.php';
@@ -35,6 +36,17 @@ abstract class BaseController
             return file_get_contents($headCode);
         } else {
             throw new \Exception("Arquivo View/code_head.php não encontrado.");
+        }
+    }
+
+    private function get_footer()
+    {
+        $footerCode = __DIR__ . '/../App/View/code_footer.php';
+
+        if( file_exists($footerCode) ){
+            return file_get_contents($footerCode);
+        } else {
+            throw new \Exception("Arquivo View/code_footer.php não encontrado.");
         }
     }
 }
