0fd51ee60c0090543d3d0b3fa1d7862aa93552f2
Marcia Silva <contato.pix@gmail.com>
2019-05-09 16:26:53 -0300

    Marcação HTML do cabeçalho

---
+++ App/View/site/header.php
@@ -0,0 +1,16 @@
+
+<!-- ========== Cabecalho ========== -->
+<header>
+    <div class="container">
+        <h1><a href="/">Segurança</a></h1>
+        <nav id="main-menu">
+            <ul>
+                <li><a href="/porto-alegre">Porto Alegre</a></li>
+                <li><a href="/canoas">Canoas</a></li>
+                <li><a href="/alvorada">Alvorada</a></li>
+                <li><a href="/gravatai">Gravataí</a></li>
+                <li><a href="/cachoeirinha">Cachoeirinha</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
