41c9b294886cd4004a9e84ef4e527f6e6b5ff976
Marcia Silva <contato.pix@gmail.com>
2019-05-09 14:10:41 -0300

    Testes com passagem de variáveis para a view

--- App/Controller/NoticiaController.php
+++ App/Controller/NoticiaController.php
@@ -7,7 +7,9 @@ class NoticiaController extends BaseController
 {
     public function index()
     {
-        echo $this->view('site/home');
+        $title = 'Título da página';
+        $nome = 'Marcia';
+        echo $this->view( 'site/home', compact('title', 'nome') );
     }
 
     public function show($id, $request)
