99fd98c9e7cf6a8860b2c8164d34f8504723b9a2
Marcia Silva <contato.pix@gmail.com>
2019-05-09 18:10:34 -0300

    Inclusão das classes CSS para debugging de layout

--- App/View/code_head.php
+++ App/View/code_head.php
@@ -7,5 +7,15 @@
     <title><?php echo isset($metaTitle) ?: 'Noticias' ?></title>
     <link href="https://fonts.googleapis.com/css?family=Oswald:400,700" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
+    <style>
+        .box1{background: #c4e6ff}
+        .box2{background: #ffc4e7}
+        .box3{background: #dcc4ff}
+        .box4{background: #c4d6ff}
+        .box5{background: #c4f5ff}
+        .box6{background: #c4ffe4}
+        .box7{background: #e3ffc4}
+        .box8{background: #fffec4}
+    </style>
 </head>
 <body>
