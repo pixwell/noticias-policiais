1fcfcba01380af7358294e32113ad0bb5a527887
Marcia Silva <contato.pix@gmail.com>
2019-05-09 19:46:38 -0300

    Progarmação do menu mobile

--- public/js/dev/custom.js
+++ public/js/dev/custom.js
@@ -1,8 +1,11 @@
 jQuery.noConflict();
-(function($){
+(function ($) {
     var $nav = $('#main-menu');
     var $toggle = $('#toggle-menu a');
+   
+    $toggle.click(function (event) {
+        event.preventDefault();
+        $nav.toggle("slow");
+    });
 
-    $nav.css('background', 'red');
-    
 })(jQuery);
