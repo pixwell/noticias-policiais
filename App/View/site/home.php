59418e32f79fad8bb82475b1b92a7b0848ac9664
Marcia Silva <contato.pix@gmail.com>
2019-05-09 14:11:11 -0300

    Testes de recebimento de variáveis passadas pelo controller

--- App/View/site/home.php
+++ App/View/site/home.php
@@ -1,2 +1,3 @@
-<h1>Home</h1>
+<h1><?php echo $title ?></h1>
+<p>Olá <?php echo $nome ?>  </p>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas assumenda quos totam quia dolores, ab deleniti modi sit quas odio enim quibusdam voluptatum consequatur repudiandae veritatis a suscipit quidem doloremque.</p>
