00c63103a0313d2a465d8b6ae1aedea2f3d9a0c6
Marcia Silva <contato.pix@gmail.com>
2019-05-09 10:34:30 -0300

    View para conteúdo da home do site

---
+++ App/View/site/home.php
@@ -0,0 +1,2 @@
+<h1>Home</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas assumenda quos totam quia dolores, ab deleniti modi sit quas odio enim quibusdam voluptatum consequatur repudiandae veritatis a suscipit quidem doloremque.</p>
