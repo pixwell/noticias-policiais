1bed1030655d1bee1c440df69250bcbecaa2e785
Marcia Silva <contato.pix@gmail.com>
2019-05-09 14:04:43 -0300

    Documentação do método view()

--- Core/BaseController.php
+++ Core/BaseController.php
@@ -6,13 +6,22 @@ abstract class BaseController
 {
     private $viewPath = __DIR__ . '/../App/View/';
 
-    protected function view($file, $variables = null)
+    /**
+     * Carrega as views
+     *
+     * @param  string $file
+     * @param  array $variables
+     *
+     * @return void
+     */
+    protected function view($file, array $variables = null)
     {
         $filePath = $this->viewPath . $file . '.php';
         if( file_exists($filePath) ){
+            $variables ? extract($variables) : null;
             include_once $filePath;
         } else {
-            \http_response_code(404);
+            http_response_code(404);
             throw new \Exception("View não encontrada.");
         }
     }
