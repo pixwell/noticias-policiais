40d9939aef4d4eaf41ae74f0b249f1800f4acdd9
Marcia Silva <contato.pix@gmail.com>
2019-05-10 17:04:55 -0300

    Criada constante para gerar URL para os Assets

--- Core/bootstrap.php
+++ Core/bootstrap.php
@@ -1,4 +1,6 @@
 <?php
+$site_root = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
+define('ASSETS', $site_root);
 //Arquivo de rotas da aplicacao
 $routes = require_once __DIR__ . '/../App/routes.php';
 //Capturando a URL do browser
