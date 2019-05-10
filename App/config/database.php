d1c2aff73df1da3e2cc4050e9e5d5d043178ae56
Marcia Silva <contato.pix@gmail.com>
2019-05-10 17:56:01 -0300

    Criado arquivos de configuração para o Banco de dados

---
+++ App/config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'driver' => 'mysql',
+    'mysql' => [
+        'host' => 'localhost',
+        'database' => 'noticias_policiais',
+        'user' => 'vagrant',
+        'password' => 'vagrant',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+    ]    
+];
