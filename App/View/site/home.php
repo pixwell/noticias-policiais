e85f31ec7a2887f0712c002ba16be4b8541e594c
Marcia Silva <contato.pix@gmail.com>
2019-05-10 15:54:18 -0300

    Retirado o tamanho do SVG do CSS e movido para o ícone através das propriedade width e height

--- App/View/site/home.php
+++ App/View/site/home.php
@@ -54,7 +54,7 @@ include 'header.php';
             <!-- Sidebar  -->
             <aside class="col-lg-3 sidebar">
                 <div class="widget mb-30">
-                    <h2><svg><use href="#boletim" /></svg>Ocorrências</h2>
+                    <h2><svg width="24" height="34"><use href="#boletim" /></svg>Ocorrências</h2>
                     <p class="mb-20">Conte-nos o que está acontecendo na sua cidade, no seu bairro, envie sua história.</p>
                     <a href="/formulario" class="btn-estilo1">Enviar ocorrência</a>
                 </div>
