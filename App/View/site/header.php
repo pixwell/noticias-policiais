21b6a2aff35a00d22a40d0516409959dcc3c2b46
Marcia Silva <contato.pix@gmail.com>
2019-05-10 12:06:16 -0300

    Atualizada as roas nos links

--- App/View/site/header.php
+++ App/View/site/header.php
@@ -10,11 +10,11 @@
         
         <nav id="main-menu">
             <ul class="menu">
-                <li><a href="/porto-alegre">Porto Alegre</a></li>
-                <li><a href="/canoas">Canoas</a></li>
-                <li><a href="/alvorada">Alvorada</a></li>
-                <li><a href="/gravatai">Gravataí</a></li>
-                <li><a href="/cachoeirinha">Cachoeirinha</a></li>
+                <li><a href="/cidade/porto-alegre">Porto Alegre</a></li>
+                <li><a href="/cidade/canoas">Canoas</a></li>
+                <li><a href="/cidade/alvorada">Alvorada</a></li>
+                <li><a href="/cidade/gravatai">Gravataí</a></li>
+                <li><a href="/cidade/cachoeirinha">Cachoeirinha</a></li>
             </ul>
         </nav>
     </div>
