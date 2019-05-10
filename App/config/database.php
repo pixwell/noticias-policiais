fd124f425fc9cf654551cbf408fbb87456c29623
Marcia Silva <contato.pix@gmail.com>
2019-05-10 19:37:52 -0300

    Alterado o login do BD

--- App/config/database.php
+++ App/config/database.php
@@ -5,7 +5,7 @@ return [
     'mysql' => [
         'host' => 'localhost',
         'dbname' => 'noticias_policiais',
-        'user' => 'vagrant',
+        'user' => 'root',
         'password' => 'vagrant',
         'charset' => 'utf8',
         'collation' => 'utf8_unicode_ci',
