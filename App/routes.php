7f5cf0f6e15cdee4633c05051323c1329f3686e9
Marcia Silva <contato.pix@gmail.com>
2019-05-13 14:34:08 -0300

    Rota para o formulário de registro de ocorrências

--- App/routes.php
+++ App/routes.php
@@ -4,6 +4,7 @@
 return [
     //['rota', 'controller', 'action'],
     ['/', 'NoticiaController', 'index'],
+    ['registrar-ocorrencia', 'NoticiaController', 'create'],
     ['noticia/{id}/show', 'NoticiaController', 'show'],
     ['cidade/{id}', 'CidadeController', 'actionCidade'],
     ['{slug}', 'slugController', 'actionSlug'],
