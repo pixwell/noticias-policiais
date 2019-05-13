738b5ac1357950811df41ec59a4d5a4d8b3de6d0
Marcia Silva <contato.pix@gmail.com>
2019-05-13 14:35:12 -0300

    Ajuste na link do botão para apontar para a rota do formulário de registro

--- App/View/site/widget-ocorrencia.php
+++ App/View/site/widget-ocorrencia.php
@@ -1,5 +1,5 @@
 <div class="widget mb-30">
     <h2><svg width="24" height="34"><use href="#boletim" /></svg>Ocorrências</h2>
     <p class="mb-20">Conte-nos o que está acontecendo na sua cidade, no seu bairro, envie sua história.</p>
-    <a href="/formulario" class="btn-estilo1">Enviar ocorrência</a>
+    <a href="/registrar-ocorrencia" class="btn-estilo1">Enviar ocorrência</a>
 </div>
