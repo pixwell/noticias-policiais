96a63e6e66db02dbf08b2b252ae21d09179fc8b2
Marcia Silva <contato.pix@gmail.com>
2019-05-13 15:09:25 -0300

    Rota para gravar ocorrência no BD

--- App/routes.php
+++ App/routes.php
@@ -5,7 +5,5 @@ return [
     //['rota', 'controller', 'action'],
     ['/', 'NoticiaController', 'index'],
     ['registrar-ocorrencia', 'NoticiaController', 'create'],
-    ['noticia/{id}/show', 'NoticiaController', 'show'],
-    ['cidade/{id}', 'CidadeController', 'actionCidade'],
-    ['{slug}', 'slugController', 'actionSlug'],
+    ['store-ocorrencia', 'NoticiaController', 'store']
 ];
