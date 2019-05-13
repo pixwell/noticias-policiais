e3e04f6d144464b7c49fb7f87fa78b1fa75f3f34
Marcia Silva <contato.pix@gmail.com>
2019-05-13 12:14:51 -0300

    View com o formulário de ocorrências

---
+++ App/View/site/form.php
@@ -0,0 +1,25 @@
+<?php 
+//Variavel reservada
+include $code_head;
+
+include 'header.php';
+?>
+
+<main>
+    <div class="container pt-50 pb-30">
+
+        <div class="row">
+            <section class="col-lg-9">
+                
+            </section>
+        
+            <!-- Sidebar  -->
+            <aside class="col-lg-3 sidebar">
+                <?php include 'widget-ocorrencia.php' ?>
+            </aside><!-- /Sidebar  -->
+        </div>
+    
+    </div>
+</main>
+
+<?php include $code_footer ?>
