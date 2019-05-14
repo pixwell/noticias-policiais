e815b1e6c3eb78336af46484ef35c35d65fdf7a3
Marcia Silva <contato.pix@gmail.com>
2019-05-14 10:00:06 -0300

    Criado a construção dinâmica da query INSERT no método insert()

--- Core/BaseModel.php
+++ Core/BaseModel.php
@@ -23,4 +23,17 @@ abstract class BaseModel
 
         return $response;
     }
+    
+    public function insert($data)
+    {
+        $dataArray = (array) $data;
+        $campos = implode(',', array_keys($dataArray));
+        $placeholder = ':' . implode(',:', array_keys($dataArray));
+        $valores = '';
+        
+        $query = 'INSERT INTO ' . $this->table . '(' . $campos . ') VALUES (' . $placeholder . ')';
+        return $query;
+        //$stmt = $pdo->prepare($query);
+        //$stmt->execute($data);
+    }
 }
