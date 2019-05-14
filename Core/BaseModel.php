a5f70dd85e54bc2883ef989261abb35490eb9827
Marcia Silva <contato.pix@gmail.com>
2019-05-14 17:49:00 -0300

    Programação do método insert()

--- Core/BaseModel.php
+++ Core/BaseModel.php
@@ -29,8 +29,7 @@ abstract class BaseModel
     public function insert($data)
     {
         //A consulta no BD retorna um OBJ, portanto ele deve ser convertido para array
-        $dataArray = (array) $data;
-        
+        $dataArray = (array) $data;        
         //Junstando as chaves do array e criando a string com os nomes dos campos
         $campos = implode(',', array_keys($dataArray));
         //Juntando as chaves e criando uma string com os placeholders
@@ -41,24 +40,13 @@ abstract class BaseModel
         $valores = array_values($dataArray);
         
         $query = 'INSERT INTO ' . $this->table . ' (' . $campos . ') VALUES (' . $placeholder . ')';
-        return $this->pdo;
-//        $stmt = $this->pdo->prepare($query);
-//        $stmt->bindValue(':author', 'Agências internacionais');
-//        $stmt->bindValue(':categories_id', 1);
-//        $stmt->bindValue(':title', 'WhatsApp detecta vulnerabilidade que permite acesso de hackers a celulares');
-//        $stmt->bindValue(':content', 'O aplicativo de mensagem instantânea WhatsApp, de propriedade do Facebook, informou na segunda-feira (13) que detectou uma vulnerabilidade em seu sistema que permitia que hackers instalassem um tipo de "spyware", um software para ter acesso a dados do aparelho, em alguns telefones. A empresa confirmou em comunicado à imprensa a informação publicada horas antes pelo "Financial Times" e pediu ao 1,5 bilhão de usuários em todo o mundo que "atualizem o aplicativo para sua versão mais recente" e mantenham durante o dia seu sistema operativo como medida de "proteção".');
-//        $response = $stmt->execute();
-//        $stmt->closeCursor();
-//        return $response;
-
+        $stmt = $this->pdo->prepare($query);
         
-//        $stmt = $this->pdo->prepare($query);
-//        
-//        for ($i = 0; $i < count($placeArray); $i++) {
-//            $stmt->bindValue($placeArray[$i], $valores[$i]);
-//        }
-//        $response = $stmt->execute();
-//        $stmt->closeCursor();
-//        return $response;
+        for ($i = 0; $i < count($placeArray); $i++) {
+            $stmt->bindValue($placeArray[$i], $valores[$i]);
+        }
+        $response = $stmt->execute();
+        $stmt->closeCursor();
+        return $response;
     }
 }
