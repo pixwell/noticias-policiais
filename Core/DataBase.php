1e790248e148a1fab33c1991beb597551f03a900
Marcia Silva <contato.pix@gmail.com>
2019-05-14 11:48:11 -0300

    Correção na inclusão do array de configuração e readequação na estrutura interna, já que a chave "driver" foi retirada do arquivo de configuração

--- Core/DataBase.php
+++ Core/DataBase.php
@@ -7,26 +7,22 @@ use PDO;
 class DataBase
 {
     public static function conn(){
-        $dbConfig = include_once __DIR__ . '/../App/config/database.php';
+        $dbConfig = include __DIR__ . '/../App/config/database.php';
+        $host = $dbConfig['host'];
+        $dbname = $dbConfig['dbname'];
+        $user = $dbConfig['user'];
+        $password = $dbConfig['password'];
+        $charset = $dbConfig['charset'];
+        $colation = $dbConfig['collation'];
 
-        if( $dbConfig['driver'] == 'mysql' ){
-            $host = $dbConfig['mysql']['host'];
-            $dbname = $dbConfig['mysql']['dbname'];
-            $user = $dbConfig['mysql']['user'];
-            $password = $dbConfig['mysql']['password'];
-            $charset = $dbConfig['mysql']['charset'];
-            $colation = $dbConfig['mysql']['collation'];
-
-            try {
-                $conn = new PDO('mysql:host=' . $host . ';dbname=' . $dbname, $user, $password);
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $conn->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES '$charset' COLLATE '$colation'");
-                $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-                return $conn;
-            } catch (\PDOException $e) {
-                throw new \Exception( $e->getMessage() );
-            }
-
-        }//if
+        try {
+            $conn = new PDO('mysql:host=' . $host . ';dbname=' . $dbname, $user, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $conn->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES '$charset' COLLATE '$colation'");
+            $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+            return $conn;
+        } catch (\PDOException $e) {
+            throw new \Exception( $e->getMessage() );
+        }
     }    
 }
