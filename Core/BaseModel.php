2fd921ae44f8aa882a43904a645c8424ee3f2b8b
Marcia Silva <contato.pix@gmail.com>
2019-05-14 11:49:03 -0300

    Retirada do método conn()

--- Core/BaseModel.php
+++ Core/BaseModel.php
@@ -12,27 +12,7 @@ abstract class BaseModel
 
     public function __construct()
     {
-        $this->pdo = $this->conn();
-    }
-    
-    public function conn(){
-        $dbConfig = include __DIR__ . '/../App/config/database.php';
-        $host = $dbConfig['host'];
-        $dbname = $dbConfig['dbname'];
-        $user = $dbConfig['user'];
-        $password = $dbConfig['password'];
-        $charset = $dbConfig['charset'];
-        $colation = $dbConfig['collation'];
-
-        try {
-            $conn = new PDO('mysql:host=' . $host . ';dbname=' . $dbname, $user, $password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $conn->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES '$charset' COLLATE '$colation'");
-            $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-            return $conn;
-        } catch (\PDOException $e) {
-            throw new \Exception( $e->getMessage() );
-        }
+        $this->pdo = DataBase::conn();
     }
     
     public function all()
@@ -61,7 +41,7 @@ abstract class BaseModel
         $valores = array_values($dataArray);
         
         $query = 'INSERT INTO ' . $this->table . ' (' . $campos . ') VALUES (' . $placeholder . ')';
-        return $this->conn();
+        return $this->pdo;
 //        $stmt = $this->pdo->prepare($query);
 //        $stmt->bindValue(':author', 'Agências internacionais');
 //        $stmt->bindValue(':categories_id', 1);
