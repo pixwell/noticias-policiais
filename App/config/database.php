e03e0a923cb735c26d5a113dbf900fd38c243a6d
Marcia Silva <contato.pix@gmail.com>
2019-05-14 11:33:21 -0300

    Alterada a estrutura do Array, retirada a chave "driver"

--- App/config/database.php
+++ App/config/database.php
@@ -1,13 +1,12 @@
 <?php
-
+/**
+ * Dados para conexao o BD MySQL
+ */
 return [
-    'driver' => 'mysql',
-    'mysql' => [
-        'host' => 'localhost',
-        'dbname' => 'noticias_policiais',
-        'user' => 'root',
-        'password' => 'vagrant',
-        'charset' => 'utf8mb4',
-        'collation' => 'utf8mb4_unicode_ci',
-    ]    
+    'host' => 'localhost',
+    'dbname' => 'noticias_policiais',
+    'user' => 'root',
+    'password' => 'vagrant',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci'    
 ];
