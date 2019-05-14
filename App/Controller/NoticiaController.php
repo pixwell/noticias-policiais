69af33a3aa22c03a7be5b8d9ca166831971fd318
Marcia Silva <contato.pix@gmail.com>
2019-05-14 10:46:08 -0300

    Programada a verificação (if) da gravação em BD

--- App/Controller/NoticiaController.php
+++ App/Controller/NoticiaController.php
@@ -3,13 +3,16 @@ namespace App\Controller;
 
 use Core\BaseController;
 use App\Model\Category;
+use App\Model\Noticia;
 
 class NoticiaController extends BaseController
 {
     private $modelCategory;
+    private $modelNoticia;
 
     public function __construct(){
         $this->modelCategory = new Category;
+        $this->modelNoticia = new Noticia;
     }
 
     public function index()
@@ -34,6 +37,16 @@ class NoticiaController extends BaseController
 
     public function store($request)
     {
-        var_dump($request);
+        $campos = $request->post;
+        if( !empty($campos->author) && !empty($campos->categories_id) && !empty($campos->title) && !empty($campos->content)){
+            $gravar = $this->modelNoticia->insert($campos);
+            if($gravar){
+                echo '<p class="status-success">Ocorrência enviada com sucesso!</p>';
+            } else {
+                echo '<p class="status-fail">Erro ao enviar ocorrência.</p>';
+            }
+        } else {
+            echo 'Erro! Por favor preencha os campos do formulário corretamente.';
+        }
     }
 }
