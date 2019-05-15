226a3b2f94d589cf3c3440ad1e25f27d7048adb7
Marcia Silva <contato.pix@gmail.com>
2019-05-15 11:06:57 -0300

    Marcação HTML da view para exibir as páginas single

---
+++ App/View/site/single.php
@@ -0,0 +1,34 @@
+<?php 
+//Variavel reservada
+include $code_head;
+
+include 'header.php';
+?>
+
+<main>
+    <div class="container pt-50 pb-30">
+
+        <div class="row">
+            <section class="col-lg-9 lista-noticias">
+                <article>
+                    <div class="details">
+                        <div class="category"><a href="#">Categoria</a></div>
+                        <div class="date">00/00/0000</div>
+                    </div>
+                    <div class="text">
+                        <h2>Título da Notícia</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, dolor, atque quae eos blanditiis quasi mollitia at accusantium numquam laudantium omnis ipsa vel asperiores dolores voluptatibus in odio sint labore!</p>
+                    </div>
+                </article>  
+            </section>
+        
+            <!-- Sidebar  -->
+            <aside class="col-lg-3 sidebar">
+                <?php include 'widget-ocorrencia.php' ?>
+            </aside><!-- /Sidebar  -->
+        </div>
+    
+    </div>
+</main>
+
+<?php include $code_footer ?>
