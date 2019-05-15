63eed8e578148bc6886edf1e1b166d592aaef5c9
Marcia Silva <contato.pix@gmail.com>
2019-05-15 09:43:27 -0300

    Correção do charset encoding, movida a configuração do charset, do $conn->setAttribute() para os options do DNS PDO

--- Core/DataBase.php
+++ Core/DataBase.php
@@ -14,11 +14,13 @@ class DataBase
         $password = $dbConfig['password'];
         $charset = $dbConfig['charset'];
         $colation = $dbConfig['collation'];
+        $options = array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES '$charset' COLLATE '$colation'"
+        );
 
         try {
-            $conn = new PDO('mysql:host=' . $host . ';dbname=' . $dbname, $user, $password);
+            $conn = new PDO('mysql:host=' . $host . ';dbname=' . $dbname, $user, $password, $options);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $conn->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES '$charset' COLLATE '$colation'");
             $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
             return $conn;
         } catch (\PDOException $e) {
