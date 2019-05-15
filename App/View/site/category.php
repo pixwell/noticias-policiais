2b421a1d90508c7677a17fa06138d2ad04a89745
Marcia Silva <contato.pix@gmail.com>
2019-05-15 19:27:35 -0300

    Inserida a barra (/) antes do slug, no link, para evitar erros na rota

---
+++ App/View/site/category.php
@@ -0,0 +1,46 @@
+<?php 
+//Variavel reservada
+include $code_head;
+
+include 'header.php';
+?>
+
+<main>
+    <div class="container pt-50 pb-30">
+
+        <div class="row">
+            <section class="col-lg-9">
+                <h2 class="mb-50">Notícias de <?php echo $titleCategoria  ?></h2>
+            
+            <?php if($noticias){ ?>
+                <!-- Lista de noticias  -->
+                <ul class="lista-noticias">
+                    <?php foreach ($noticias as $noticia) { ?>
+                    <li>
+                        <article>
+                            <div class="details">
+                                <div class="date"><?php echo date( 'd/m/Y' , strtotime( $noticia->created_at ) ); ?></div>
+                            </div>
+                            <div class="text">
+                                <h3><a href="/<?php echo $noticia->slug ?>"><?php echo $noticia->title ?></a></h3>
+                                <?php limita_texto( strip_tags($noticia->content), 250 ) ?>
+                            </div>
+                        </article>
+                    </li>
+                    <?php } ?>                   
+                </ul><!-- /Lista de noticias  -->
+            <?php } else { ?>
+                <p>Nenhuma notícias encontrada.</p>
+            <?php }//if ?>   
+            </section>
+        
+            <!-- Sidebar  -->
+            <aside class="col-lg-3 sidebar">
+                <?php include 'widget-ocorrencia.php' ?>
+            </aside><!-- /Sidebar  -->
+        </div>
+    
+    </div>
+</main>
+
+<?php include $code_footer ?>
