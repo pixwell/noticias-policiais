ae2763c228888a8caaa5e8b42ad908d789e87231
Marcia Silva <contato.pix@gmail.com>
2019-05-16 16:33:09 -0300

    Inserida Trait Authenticate

--- App/Controller/UserController.php
+++ App/Controller/UserController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use Core\BaseController;
+use Core\Authenticate;
 use App\Model\User;
 
 class UserController extends BaseController
 {
+    use Authenticate;
+    
     private $userModel;
     private $salt = '?%#tzT$30XnC$NMM/WgTho;DHIUV4z2XR@E?-/g}{Q[vCY;/Za[>^m1P0o=@=}6R';
     
