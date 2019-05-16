510ad6534a2140ccdf0d4f4d043d51032a9f4594
Marcia Silva <contato.pix@gmail.com>
2019-05-16 13:52:52 -0300

    Criado o controller para o painel admin

---
+++ App/Controller/AdminController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Core\BaseController;
+
+class AdminController extends BaseController
+{
+    public function index()
+    {
+       echo '<h1>Painel de controle</h1>'; 
+    }
+}
