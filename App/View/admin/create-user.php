28ad16e289f21fe2deb5be3185e4f8ffb464ba15
Marcia Silva <contato.pix@gmail.com>
2019-05-16 16:49:09 -0300

    Marcação HTML do formulário de criação de usuário

---
+++ App/View/admin/create-user.php
@@ -0,0 +1,29 @@
+<?php 
+//Variavel reservada
+include $code_head;
+?>
+
+<main>
+    <div class="container pt-50 pb-30">
+
+        <div class="row d-flex justify-content-center align-content-center">
+            <section class="col-lg-5">
+                <div class="widget">
+                    <h2 class="mb-20 text-center">Criar Usuário</h2>
+                    <form id="form-create-user">
+                        <input type="text" name="nome" id="nome" placeholder="Nome">
+                        <input type="text" name="login" id="login" placeholder="Login">
+                        <input type="password" name="pass1" id="pass1" placeholder="Senha">
+                        <input type="password" name="pass2" id="pass2" placeholder="Confirmar Senha">
+                        <input type="submit" value="Enviar" class="btn-estilo1">
+                    </form>                    
+                </div>
+                <div id="status-login"></div>
+                <p class="mt-20"><svg width="22" height="22" style="margin-right: 8px"><use href="#home" /></svg> Voltar para <a href="/">a Home</a></p>
+            </section>
+        </div>
+    
+    </div>
+</main>
+
+<?php include $code_footer ?>
