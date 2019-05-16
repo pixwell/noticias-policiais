58f44d70b6b35fab889992d63ecbad1f024eaca5
Marcia Silva <contato.pix@gmail.com>
2019-05-16 16:52:33 -0300

    Rota para criação de usuários

--- App/routes.php
+++ App/routes.php
@@ -10,6 +10,7 @@ return [
     ['login', 'UserController', 'login'],
     ['auth', 'UserController', 'auth'],
     ['create-user', 'UserController', 'createUser'],
+    ['store-user', 'UserController', 'storeUser'],
     
     ['admin', 'AdminController', 'index'],
     ['cidade/{slug}', 'NoticiaController', 'category'],
