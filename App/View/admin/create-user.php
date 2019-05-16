53972a2147dc10b58722de2437e11f53945095fb
Marcia Silva <contato.pix@gmail.com>
2019-05-16 17:09:21 -0300

    Inserido limite mínimo de caracteres nos campos input Nome e Login

--- App/View/admin/create-user.php
+++ App/View/admin/create-user.php
@@ -11,8 +11,8 @@ include $code_head;
                 <div class="widget">
                     <h2 class="mb-20 text-center">Criar Usuário</h2>
                     <form id="form-create-user">
-                        <input type="text" name="nome" id="nome" placeholder="Nome">
-                        <input type="text" name="login" id="login" placeholder="Login">
+                        <input type="text" name="nome" id="nome" minlength="3" placeholder="Nome">
+                        <input type="text" name="login" id="login" minlength="6" placeholder="Login">
                         <input type="password" name="pass1" id="pass1" placeholder="Senha">
                         <input type="password" name="pass2" id="pass2" placeholder="Confirmar Senha">
                         <input type="submit" value="Enviar" class="btn-estilo1">
