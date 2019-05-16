c68042abcd579140940b2d5f4398790cbbfce338
Marcia Silva <contato.pix@gmail.com>
2019-05-16 19:04:54 -0300

    Comparando os dados vindos do formulário com os dados do BD

--- Core/Authenticate.php
+++ Core/Authenticate.php
@@ -2,10 +2,17 @@
 
 namespace Core;
 
+use App\Model\User;
+
 trait Authenticate
 {
+    private $userModel;
     private $redirectTo = '/admin';
-        
+    
+    public function __construct(){
+        $this->userModel = new User;
+    }
+
     public function login()
     {
         $title = 'Login';
@@ -15,12 +22,31 @@ trait Authenticate
     public function auth($request)
     {
         $campos = $request->post;
-        
-        if( !empty($campos->login) &&!empty($campos->password) ){
-            echo json_encode([
-                'status' => true,
-                'redirect' => $this->redirectTo
-            ]);
+        //Se todos os campos estiverem preenchidos
+        if( !empty($campos->login) && !empty($campos->password) ){
+            $userData = $this->userModel->findWhere(['login', $campos->login]);
+            //Achou algum login? Comparar as senhas
+            if( $userData && password_verify($campos->password, $userData[0]->password) ){                
+                //Autenticar
+                 
+                //Redirecionar
+                echo json_encode([
+                    'status' => true,
+                    'mensagem' => '',
+                    'redirect' => $this->redirectTo
+                ]);
+            } else {
+                //Exibir mensagem de erro
+                echo json_encode([
+                    'status' => false,
+                    'mensagem' => '<p class="status-fail">Login ou senha estão incorretos.</p>',
+                    'redirect' => ''
+                ]);
+            }
+        } else {
+            //Se nao estiverem preenchidos
+            echo '<p class="status-fail">Por favor preencha corretamente os campos do formulário.</p>';
         }
+        
     }
 }
