e483bac67a691c60509d0e145554a661088c4f5d
Marcia Silva <contato.pix@gmail.com>
2019-05-16 16:27:21 -0300

    Criada propriedade $salt para as senhas

--- App/Controller/UserController.php
+++ App/Controller/UserController.php
@@ -8,6 +8,7 @@ use App\Model\User;
 class UserController extends BaseController
 {
     private $userModel;
+    private $salt = '?%#tzT$30XnC$NMM/WgTho;DHIUV4z2XR@E?-/g}{Q[vCY;/Za[>^m1P0o=@=}6R';
     
     public function __construct()
     {
