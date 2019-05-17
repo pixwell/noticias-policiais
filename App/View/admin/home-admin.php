a53b19838bb5c58914f4367a46afcbc912b0c3fb
Marcia Silva <contato.pix@gmail.com>
2019-05-17 19:24:24 -0300

    Programação da view

--- App/View/admin/home-admin.php
+++ App/View/admin/home-admin.php
@@ -5,29 +5,34 @@ include 'header-admin.php';
 
 <main>
     <div class="container pt-50 pb-30">
-        <!-- Lista de noticias  -->
-        <ul class="lista-noticias-admin">
-            <li>
-                <article class="row">
-                    <div class="text col-md-8 col-lg-9 mb-20">
-                        <h2><a href="/#">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a></h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, ipsam, possimus magnam cupiditate deserunt veniam natus delectus quae dolorum iure eum nisi dolores non maiores alias reiciendis repellat. Dolorum, reiciendis!</p>
-                    </div>
-                    <div class="details col-md-4 col-lg-3 mb-20">
-                        <ul>
-                            <li><svg><use href="#user" /></svg> João</li>
-                            <li><svg><use href="#folder" /></svg> Alvorada</li>
-                            <li><svg><use href="#clock" /></svg> 00/00/2019</li>
-                        </ul>
-                        <div class="mt-20">
-                            <a href="#" class="btn-success" style="margin-right: 5px"><svg><use href="#checkbox" /></svg> Aprovar</a>
-                            <a href="#" class="btn-fail"><svg><use href="#trash" /></svg> Deletar</a>
+        <?php if ($noticias) : ?>
+            <!-- Lista de noticias  -->
+            <ul class="lista-noticias-admin">
+            <?php foreach ($noticias as $noticia) : ?>                
+                <li>
+                    <article class="row">
+                        <div class="text col-md-8 col-lg-9 mb-20">
+                            <h2><a href="/admin/<?php echo $noticia->slug ?>"><?php echo $noticia->title ?></a></h2>
+                            <?php limita_texto( strip_tags($noticia->content), 250 ) ?>
                         </div>
-                    </div>
-                </article>
-            </li>                
-        </ul><!-- /Lista de noticias  -->
-
+                        <div class="details col-md-4 col-lg-3 mb-20">
+                            <ul>
+                                <li><svg><use href="#user" /></svg> <?php echo $noticia->author ?></li>
+                                <li><svg><use href="#folder" /></svg> <?php echo $categoryList[$noticia->categories_id]['title'] ?></li>
+                                <li><svg><use href="#clock" /></svg> <?php echo date( 'd/m/Y' , strtotime( $noticia->created_at ) ); ?></li>
+                            </ul>
+                            <div class="mt-20">
+                                <a href="#" class="btn-success" style="margin-right: 5px"><svg><use href="#checkbox" /></svg> Aprovar</a>
+                                <a href="#" class="btn-fail"><svg><use href="#trash" /></svg> Deletar</a>
+                            </div>
+                        </div>
+                    </article>
+                </li>  
+            <?php endforeach; ?>
+            </ul><!-- /Lista de noticias  -->    
+        <?php else : ?>
+            <p>Nenhuma notícia registrada ainda.</p>
+        <?php endif; ?>
     </div>
 </main>
 
