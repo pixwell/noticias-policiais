464da8b4c97db9fb3ef6cf50579d96bd5e7e6c12
Marcia Silva <contato.pix@gmail.com>
2019-05-17 10:48:47 -0300

    Marcação HTML do header

---
+++ App/View/admin/header-admin.php
@@ -0,0 +1,18 @@
+
+<!-- ========== Cabecalho ========== -->
+<header id="header">
+    <div class="container">
+        <h1><a href="/">Admin</a></h1>
+
+        <div id="toggle-menu">
+            <a href="#"><svg><use href="#menu" /></svg></a>
+        </div>
+        
+        <nav id="main-menu">
+            <ul class="menu">
+                <li><span style="margin-right: 8px">Você está logado como, <strong>Fulano da Silva</strong></span></li>
+                <li><a href="/cidade/porto-alegre" class="btn-estilo1">Deslogar</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
