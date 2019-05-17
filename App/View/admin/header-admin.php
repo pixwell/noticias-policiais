eb538fe8e4f226ffc215013635446b06bf935e10
Marcia Silva <contato.pix@gmail.com>
2019-05-17 17:21:41 -0300

    Alterado o nome da função para exibir o nome do usuário

--- App/View/admin/header-admin.php
+++ App/View/admin/header-admin.php
@@ -10,7 +10,7 @@
         
         <nav id="main-menu">
             <ul class="menu">
-                <li><span style="margin-right: 8px">Você está logado como, <strong><?php echo auth_check() ? user_name() : '' ?></strong></span></li>
+                <li><span style="margin-right: 8px">Você está logado como, <strong><?php echo user_name() ?></strong></span></li>
                 <li>
                     <a href="/logout" class="btn-estilo1"><svg width="16" height="14" style="margin-right: 5px">
                         <use href="#power" /></svg>Deslogar
