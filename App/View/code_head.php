24b3fbac7acdfb921fcd537970385d48b4fef7f3
Marcia Silva <contato.pix@gmail.com>
2019-05-17 10:23:25 -0300

    Ajuste no operador ternário para exibir o título da página

--- App/View/code_head.php
+++ App/View/code_head.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo isset($metaTitle) ?: 'Noticias' ?></title>
+    <title><?php echo isset($metaTitle) ? $metaTitle : 'Noticias' ?></title>
     <link href="https://fonts.googleapis.com/css?family=Oswald:400,700" rel="stylesheet">
     <link rel="shortcut icon" href="<?php echo ASSETS ?>/images/favicon.png">
     <link rel="stylesheet" href="<?php echo ASSETS ?>/css/style.css">
