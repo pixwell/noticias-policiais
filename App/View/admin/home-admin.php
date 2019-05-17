27fa93a68e5545c380b30c7548ae516bd0a87b49
Marcia Silva <contato.pix@gmail.com>
2019-05-17 10:31:12 -0300

    Home da administração

---
+++ App/View/admin/home-admin.php
@@ -0,0 +1,27 @@
+<?php
+//Variavel reservada
+include $code_head;
+?>
+
+<main>
+    <div class="container pt-50 pb-30">
+
+        <!-- Lista de noticias  -->
+        <ul class="lista-noticias">
+                <li>
+                    <article>
+                        <div class="details">
+                            <div class="date">00/00/00</div>
+                        </div>
+                        <div class="text">
+                            <h3><a href="/#">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a></h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, ipsam, possimus magnam cupiditate deserunt veniam natus delectus quae dolorum iure eum nisi dolores non maiores alias reiciendis repellat. Dolorum, reiciendis!</p>
+                        </div>
+                    </article>
+                </li>                  
+        </ul><!-- /Lista de noticias  -->
+
+    </div>
+</main>
+
+<?php include $code_footer ?>
