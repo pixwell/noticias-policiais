12530bdbd7a18661201d929ba51df8ebdb593551
Marcia Silva <contato.pix@gmail.com>
2019-05-17 15:16:19 -0300

    Envio das informações da sessão para a view home-admin.php

--- App/Controller/AdminController.php
+++ App/Controller/AdminController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use Core\BaseController;
+use Core\Session;
 
 class AdminController extends BaseController
 {
     public function index()
     {
         $metaTitle = 'Home Administração';
-        echo $this->view('admin/home-admin', compact('metaTitle'));
+        $sessao = Session::get('user');
+        echo $this->view('admin/home-admin', compact('metaTitle', 'sessao'));
     }
 }
