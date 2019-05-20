0f3335f42b70e96cf72c2fde072e2f0564ee1cbb
Marcia Silva <contato.pix@gmail.com>
2019-05-20 16:51:23 -0300

    Retirado o link e incluído data-id para armazenar os IDs dos posts

--- App/View/admin/home-admin.php
+++ App/View/admin/home-admin.php
@@ -23,9 +23,9 @@ include 'header-admin.php';
                             </ul>
                             <div class="mt-20">
                                 <?php if($noticia->active){ ?>                                
-                                <a href="/admin/<?php echo $noticia->id ?>/change-status" class="btn-processing" id="aprovado" style="margin-right: 5px"><svg><use href="#checkbox" /></svg> Aprovado</a>
+                                <a href="#" data-id="<?php echo $noticia->id ?>" class="btn-status btn-processing" style="margin-right: 5px"><svg><use href="#checkbox" /></svg> Aprovado</a>
                                 <?php } else { ?>
-                                <a href="/admin/<?php echo $noticia->id ?>/change-status" id="aprovar" class="btn-success" style="margin-right: 5px"><svg><use href="#checkbox" /></svg> Aprovar</a>
+                                <a href="#" data-id="<?php echo $noticia->id ?>" class="btn-status btn-success" style="margin-right: 5px"><svg><use href="#checkbox" /></svg> Aprovar</a>
                                 <?php } ?>
                                 <a href="/admin/<?php echo $noticia->id ?>/delete" class="btn-fail"><svg><use href="#trash" /></svg> Deletar</a>
                             </div>
