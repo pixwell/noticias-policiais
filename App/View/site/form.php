9834f880438d905218291b59e459fbad1d915999
Marcia Silva <contato.pix@gmail.com>
2019-05-22 15:22:03 -0300

    Alteração nos nomes e ID dos campos, retirada a parte final do texto "-ocorrencia"

--- App/View/site/form.php
+++ App/View/site/form.php
@@ -10,28 +10,30 @@ include 'header.php';
             <section class="col-lg-9 mb-30">
                 <h2>Registre sua ocorrência</h2>
                 <p class="mb-50">Conte-nos o que está acontecendo na sua cidade, no seu bairro, envie sua história.</p>
+                
                 <form id="form-registro">
-                    <label for="nome-ocorrencia">Seu Nome</label>
-                    <input type="text" name="author" id="nome-ocorrencia">
+                    <label for="nome">Seu Nome</label>
+                    <input type="text" name="nome" id="nome">
 
-                    <label for="cidade-ocorrencia">Sua cidade</label>
-                    <select name="categories_id" id="cidade-ocorrencia">
+                    <label for="cidade">Sua cidade</label>
+                    <select name="cidade" id="cidade">
                         <option value="">Selecione ...</option>
                         <?php foreach ($cidades as $cidade) { ?>
                             <option value="<?php echo $cidade->id ?>"><?php echo $cidade->title ?></option>
                         <?php } ?>
                     </select>
 
-                    <label for="titulo-ocorrencia">Título da ocorrência</label>
-                    <input type="text" name="title" id="titulo-ocorrencia">
+                    <label for="titulo">Título da ocorrência</label>
+                    <input type="text" name="titulo" id="titulo">
 
                     <label for="texto">Texto</label>
-                    <textarea name="content" id="texto-ocorrencia" cols="30" rows="10"></textarea>
+                    <textarea name="texto" id="texto" cols="30" rows="10"></textarea>
 
                     <div id="status-envio-ocorrencia" class="mt-20 mb-20"></div>
 
                     <input type="submit" value="Enviar ocorrência" class="btn-estilo1">
                 </form>
+                
             </section>
         
             <!-- Sidebar  -->
