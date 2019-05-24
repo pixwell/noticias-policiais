de8d83002800b61be3c4cbeeaac9728b1b6a62ac
Marcia Silva <contato.pix@gmail.com>
2019-05-24 19:29:37 -0300

    Programação dos links da paginação

--- Core/Pagination.php
+++ Core/Pagination.php
@@ -33,50 +33,21 @@ class Pagination
         
     public function paginationLinks()
     {
-        return '###Paginação###';
-    }
-    
-//    public function paginationLinks($pages = '', $range = 4);
-//    {
-//        $showitems = ($range * 2)+1;        
-//        $paged = $this->currentPage;
-//        
-//        if( empty($paged) ) {
-//            $paged = 1;
-//        }
-//        
-//        if($pages == ''){
-//            $pages = $this->postPerPage;
-//            if(!$pages){
-//                $pages = 1;
-//            }
-//        }
-//        
-//        $navHtml = '';        
-//        if(1 != $pages){
-//            $navHtml = '<ul class="pagination">';
-//            if($paged > 2 && $paged > $range+1 && $showitems < $pages){
-//                $navHtml .= ' <li class="page-item active"><a href="?page='.($paged - 1).'" class="page-link">&laquo;</a></li>';
-//            }
-//            if($paged > 6 && $showitems < $pages){
-//                $navHtml .= ' <li class="page-item"><a href="?page=1" class="page-link">1</a></li>';
-//            }
-//            for ($i=1; $i <= $pages; $i++){
-//                if (1 != $pages &&( !($i >= $paged+$range+1 || $i <= $paged-$range-1) || $pages <= $showitems )){
-//                    $navHtml .= ($paged == $i)? '<li class="page-item active"> <a class="page-link" href="#">' . $i . '</a></li>' : ' <li class="page-item"><a href="?page=' . $i . '" class="page-link">' . $i . '</a></li>';
-//                }
-//            }
-//            if ($paged < $pages-1 &&  $paged+$range-1 < $pages && $showitems < $pages) {
-//                $navHtml .= '<li class="page-item"><a href="?page=' . $pages . '" class="page-link"> ... ' . $pages . '</a></li>';
-//            }
-//            if ($paged < $pages && $showitems < $pages) {
-//                $navHtml .= '<li class="page-item  active"><a href="?page=' . ($paged + 1) . '" class="page-link">&raquo;</a></li>';
-//            }
-//            $navHtml .= "</ul>\n";
-//        }
-//        
-//        return $navHtml;
-//
-//    }
-//    
+        $totalLinks = ceil($this->totalRecords / $this->postPerPage);
+        $navHtml = '';
+        if( $totalLinks > 1){
+            $navHtml = '<ul class="pagination">';
+            for ($i = 1; $i <= $totalLinks; $i++){
+                if( $this->currentPage == $i){
+                    $class = 'page-item active';
+                } else {
+                    $class = 'page-item';
+                }
+                
+                $navHtml .= '<li class="' . $class . '"><a href="?page=' . $i . '" class="page-link">' . $i . "</a></li>\n";
+            }            
+            $navHtml .= "</ul>\n";
+        }
+        return $navHtml;
+    }   
 }
