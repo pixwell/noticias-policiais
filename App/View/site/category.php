23d29555b06d562ef2be43d6573f4bef0f790041
Marcia Silva <contato.pix@gmail.com>
2019-05-27 10:58:17 -0300

    nclusão dos links da paginação

--- App/View/site/category.php
+++ App/View/site/category.php
@@ -27,6 +27,9 @@ include 'header.php';
                     </li>
                     <?php } ?>                   
                 </ul><!-- /Lista de noticias  -->
+                
+                <nav><?php echo $navPaginacao ?></nav>
+                
             <?php } else { ?>
                 <p>Nenhuma notícias encontrada.</p>
             <?php }//if ?>   
